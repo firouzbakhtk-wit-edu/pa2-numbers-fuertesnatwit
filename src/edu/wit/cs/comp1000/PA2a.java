d87722ab59e23bb12ea7e27ab0eebdc5c4b73551
Nesh Fuertes <fuertesn@wit.edu>
2024-10-02 09:12:06 -0400

    Added code to both PA2a and PA2b along with comments that explain sections of code. Both classes passed the allotted JUnit tests.

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,133 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA2a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		
+		/*
+		 * Initializing Scanner + Vars for holding
+		 */
+		Scanner input = new Scanner(System.in);
+		int posCount = 0;
+		int negCount = 0;
+		int posSum = 0;
+		int negSum = 0;
+		double posAvg = 0;
+		double negAvg = 0;
+
+		System.out.printf("Enter five whole numbers: ");
+	/*
+	 * FIRST NUMBER
+	 */
+		int a = input.nextInt();
+		if(a > 0) {
+			posCount++;
+			posSum = posSum + a;
+		} else {
+			negCount++;
+			negSum = negSum + a;
+		}
+	/*
+	 * SECOND NUMBER
+	 */
+		int b = input.nextInt();
+		if(b > 0) {
+			posCount++;
+			posSum = posSum + b;
+		} else {
+			negCount++;
+			negSum = negSum + b;
+		}
+	/*
+	 * THIRD NUMBER
+	 */
+		int c = input.nextInt();
+		if(c > 0) {
+			posCount++;
+			posSum = posSum + c;
+		} else {
+			negCount++;
+			negSum = negSum + c;
+		}
+	/*
+	 * FOURTH NUMBER 
+	 */
+		int d = input.nextInt();
+		if(d > 0) {
+			posCount++;
+			posSum = posSum + d;
+		} else {
+			negCount++;
+			negSum = negSum + d;
+		}
+	/*
+	 * FIFTH NUMBER
+	 */
+		int e = input.nextInt();
+		if(e > 0) {
+			posCount++;
+			posSum = posSum + e;
+		} else {
+			negCount++;
+			negSum = negSum + e;
+		}
+		
+	/*
+	 * Developing averages and whole number sum
+	 */
+		int sum = a + b + c + d + e;
+		double avg= sum/ 5.0;
+		if(posCount > 0) {
+			posAvg = (double)posSum/posCount;
+		}
+		if(negCount > 0) {
+			negAvg = (double)negSum/negCount;
+		}
+		
+	/*
+	 * PRINTING & DETERMINING WHICH MESSAGE TO SEND
+	 */
+		
+		if(posCount == 1) {
+			System.out.printf("The sum of the %d positive number: %d%n", posCount, posSum); 
+		} else {
+			System.out.printf("The sum of the %d positive numbers: %d%n", posCount, posSum); 
+		}
+		
+		if(negCount == 1) {
+			System.out.printf("The sum of the %d non-positive number: %d%n", negCount, negSum);
+		} else {
+			System.out.printf("The sum of the %d non-positive numbers: %d%n", negCount, negSum);
+		}
+		
+		System.out.printf("The sum of the 5 numbers: %d%n", sum);
+		/*
+		 * AVG OF POS NUMBERS
+		 */
+		if(posCount == 1) {
+			System.out.printf("The average of the %d positive number: %.2f%n", posCount, posAvg);
+		} else {
+			System.out.printf("The average of the %d positive numbers: %.2f%n", posCount, posAvg);
+		}
+		
+		/*
+		 * AVG OF NEG NUMBERS
+		 */
+		if(negCount == 1) {
+			System.out.printf("The average of the %d non-positive number: %.2f%n", negCount, negAvg);
+		} else {
+			System.out.printf("The average of the %d non-positive numbers: %.2f%n", negCount, negAvg);
+		}
+		
+		System.out.printf("The average of the 5 numbers: %.2f%n", avg);
+		
+		
+		input.close();
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,44 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA2b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		/*
+		 * Initializing Scanner + a b c vars
+		 */
+		Scanner input = new Scanner(System.in);
+		System.out.printf("Enter a b c: ");
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		double c = input.nextDouble();
+		
+		/*
+		 * checking discrim
+		 */
+		double discrim = (b*b) - (4 * a * c);
+		
+		if(discrim < 0) {
+			System.out.printf("Roots: imaginary%n");
+		} else if(discrim == 0) {
+			double root = -1 * b /(2 * a);
+			System.out.printf("Root: %.2f%n", root);
+		} else {
+			double root1 = ((-1 * b)+ Math.sqrt(discrim))/(2 * a);
+			double root2 = ((-1 * b)- Math.sqrt(discrim))/(2 * a); 
+			
+			if(root1 < root2) {
+				System.out.printf("Roots: %.2f, %.2f%n", root1, root2);
+			} else {
+				System.out.printf("Roots: %.2f, %.2f%n", root2, root1);
+			}
+		}
+		
+		input.close();
 	}
 
 }
